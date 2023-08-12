2106ccce053184a1dc5b8c43030f98fdf60f8623
Evgeniy <Evgeniy_900@mail.ru>
2023-08-12 19:28:47 +0300

    создан объект personalMovie

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,33 @@
 
 "use strict"
 
+const numberOfFilms = +prompt("Укажите количество фильмов которые вы смотрели","");
+
+const personalMovie = {
+
+    count: numberOfFilms,
+
+    movies: {},
+
+    actors: {},
+
+    genres: [],
+
+    private: false
+
+};
+
+const one = prompt('Введите один из последних просмотренных фильмов?',''),
+    
+      two = +prompt('Какую оценку поставите фильму?',''),
+
+      tree = prompt('Введите один из последних просмотренных фильмов?',''),
+
+      four = +prompt('Какую оценку поставите фильму?','');
+
+      personalMovie.movies[one] = two;
+
+      personalMovie.movies[tree] = four;
+
+      console.log(personalMovie);
+
