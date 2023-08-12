6b97f6bcff255091af89a1f79c12cbdae44b68c2
Evgeniy <Evgeniy_900@mail.ru>
2023-08-12 20:28:09 +0300

    добавлены условия и цикл

--- JS/script.js
+++ JS/script.js
@@ -17,13 +17,13 @@ const personalMovie = {
 
 };
 
-for(let i = 0; i < 2; i ++){
+for (let i = 0; i < 2; i ++) {
     
     const oneQuestion = prompt('Введите один из последних просмотренных фильмов?',''),
     
     twoQuestion = +prompt('Какую оценку поставите фильму?','');
 
-    if (oneQuestion != null && twoQuestion != null && oneQuestion != '' && twoQuestion != '' && oneQuestion.length < 30){
+    if (oneQuestion != null && twoQuestion != null && oneQuestion != '' && twoQuestion != '' && oneQuestion.length < 30) {
         
         personalMovie.movies[oneQuestion] = twoQuestion;
 
@@ -32,9 +32,26 @@ for(let i = 0; i < 2; i ++){
         console.log('Ошибка ввода данных!');
 
         i --;
-        
+
     }
 
+}
+
+if (personalMovie.count < 10) {
+
+    console.log('Вы просмотрели мало фильмов!');
+
+} else if (personalMovie.count >= 10 && personalMovie.count < 35) {
+
+    console.log('Вы любитель кинематографа!');
+
+} else if (personalMovie.count >= 30 && personalMovie.count < 55) {
+
+    console.log('Вы киноман!');
+
+} else {
+
+    console.log('Ошибка');
 }
 
 console.log(personalMovie);
